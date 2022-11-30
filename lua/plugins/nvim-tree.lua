d625d21dfa897aa086cd8d44ff30be8f912c3d3a
zhouxinke <xinke.zhou@cyclone-robotics.com>
2022-11-30 17:39:24 +0800

    fix shell in win32

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,7 +1,11 @@
 require('nvim-tree').setup {
-    open_on_setup = true,
-    open_on_setup_file = true,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    hijack_netrw = true,
     view = {
         side = 'left'
-    }
+    },
+    git = {
+        ignore = false,
+    },
 }
--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -10,6 +10,9 @@ vim.o.backspace = "indent,eol,start" -- backspace works on every char in insert 
 vim.o.clipboard = "unnamedplus" -- yanking/deleting operations copy to the system clipboard
 vim.o.autoread = true -- set to auto read when a file is changed from the outside
 vim.o.splitright = true
+if vim.fn.has('win32') == 1 then
+    vim.o.shell = 'cmd'
+end
 
 -------------------
 -- neovide
