64f2d6f5ac5350572bd701139561dd5dc9923220
jaysinco <jaysinco@163.com>
2022-12-02 22:13:40 +0800

    fix scrollbar color

--- lua/plugins/nvim-scrollbar.lua
+++ lua/plugins/nvim-scrollbar.lua
@@ -1,15 +1,16 @@
 require("scrollbar").setup {
+    show_in_active_only = true,
     handle = {
         text = " ",
-        color = "gray",
+        color = "#444341",
         hide_if_all_visible = true,
     },
     marks = {
         Search = { text = { "-", "-" }, priority = 0, color = "orange" },
-        Error  = { text = { "-", "-" }, priority = 1, color = "red" },
-        Warn   = { text = { "-", "-" }, priority = 2, color = "yellow" },
-        Info   = { text = { "-", "-" }, priority = 3, color = "blue" },
-        Hint   = { text = { "-", "-" }, priority = 4, color = "green" },
-        Misc   = { text = { "-", "-" }, priority = 5, color = "purple" },
+        Error  = { text = { " ", " " }, priority = 1, color = "red" },
+        Warn   = { text = { " ", " " }, priority = 2, color = "yellow" },
+        Info   = { text = { " ", " " }, priority = 3, color = "blue" },
+        Hint   = { text = { " ", " " }, priority = 4, color = "green" },
+        Misc   = { text = { " ", " " }, priority = 5, color = "purple" },
     },
 }
