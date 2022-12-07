e94d6fc81603f68c4905a6df9dbcccb56bdb0bde
jaysinco <jaysinco@163.com>
2022-12-07 17:56:54 +0800

    change session keymap

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -23,20 +23,20 @@ db.custom_center = {
     {
         icon = '  ',
         desc = 'Load recently latest session             ',
-        shortcut = 'sa',
+        shortcut = ',sa',
         action ='SessionManager load_last_session'
     },
     {
         icon = '  ',
         desc = 'Select and load session                  ',
         action =  'SessionManager load_session',
-        shortcut = 'sl'
+        shortcut = ',sl'
     },
     {
         icon = '  ',
         desc = 'Select and delete session                ',
         action =  'SessionManager delete_session',
-        shortcut = 'sd'
+        shortcut = ',sd'
     },
 }
 
