a872e7731dbf2aab66d39fed823d6c3cb7f080ba
jaysinco <jaysinco@163.com>
2022-12-08 22:29:07 +0800

    add outline fold depth

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -16,27 +16,6 @@ local on_attach = function(client, bufnr)
     lsp_status.on_attach(client)
 end
 
--- lspconfig['sumneko_lua'].setup {
---     capabilities = capabilities,
---     on_attach = on_attach,
---     settings = {
---         Lua = {
---             runtime = {
---                 version = 'LuaJIT',
---             },
---             diagnostics = {
---                 globals = { 'vim' },
---             },
---             workspace = {
---                 library = vim.api.nvim_get_runtime_file("", true),
---             },
---             telemetry = {
---                 enable = false,
---             },
---         },
---     },
--- }
-
 lspconfig['clangd'].setup {
     capabilities = capabilities,
     on_attach = on_attach,
--- lua/plugins/symbols-outline.lua
+++ lua/plugins/symbols-outline.lua
@@ -1,4 +1,5 @@
 require('symbols-outline').setup {
     highlight_hovered_item = false,
     auto_preview = false,
+    autofold_depth = 1,
 }
