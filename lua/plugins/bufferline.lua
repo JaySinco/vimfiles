de0c16e40f946f894372e44965ce7faf2b61b142
jaysinco <jaysinco@163.com>
2022-12-10 20:44:19 +0800

    disable buffer close icon

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,8 @@
 require("bufferline").setup {
     options = {
         always_show_bufferline = true,
+        show_buffer_close_icons = false,
+        right_mouse_command = "bp|bd#",
         offsets = {
             {
                 filetype = "NvimTree",
