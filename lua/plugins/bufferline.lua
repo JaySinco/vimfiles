40e112b1654de8cc8b8324d417b499abc7fcb437
jaysinco <jaysinco@163.com>
2022-12-10 18:34:42 +0800

    setup bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,1 +1,14 @@
-require("bufferline").setup {}
+require("bufferline").setup {
+    options = {
+        always_show_bufferline = true,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = vim.fn.getcwd,
+                highlight = "Directory",
+                text_align = "left",
+                separator = false, -- use a "true" to enable the default, or set your own character
+            }
+        }
+    }
+}
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,7 +3,8 @@ require('nvim-tree').setup {
     open_on_setup_file = false,
     hijack_netrw = true,
     view = {
-        side = 'left'
+        side = 'left',
+        hide_root_folder = true,
     },
     git = {
         enable = false,
