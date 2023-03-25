2785fcd494b33586ac62a0b9feaa457715a8e114
jaysinco <jaysinco@163.com>
2023-03-25 23:24:45 +0800

    add gdb key mapping

--- sync-site.sh
+++ sync-site.sh
@@ -7,5 +7,5 @@ git_root="$(git rev-parse --show-toplevel)"
 os_arch=`arch`
 
 pushd $HOME/.local/share/nvim
-zip -r -q $MY_SOURCE_REPO/nvim-data-site-v0.7.2-linux-$os_arch.zip ./site 
+zip -FSr $MY_SOURCE_REPO/nvim-data-site-v0.7.2-linux-$os_arch.zip ./site 
 
